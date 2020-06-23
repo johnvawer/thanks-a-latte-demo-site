ea83ac1f5f59633611e72af9a03ba0e9623850fd
John Vawer <john.vawer@holidayextras.com>
2020-06-23 15:45:42 +0100

    Added modal overlay and click listener to body to close basket

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 let isVisible = false;
 
-function toggleBasket() {
+function hideBasket() {
   const basketView = document.getElementById('basket-view');
+  const wrapper = document.getElementById('page-wrapper');
 
-  if (isVisible) {
-    basketView.classList.remove('basket-is-visible');
-  } else {
-    basketView.classList.add('basket-is-visible');
+  wrapper.classList.remove('modal-overlay');
+  basketView.classList.remove('basket-is-visible');
+}
+
+function showBasket() {
+  const basketView = document.getElementById('basket-view');
+  const wrapper = document.getElementById('page-wrapper');
+
+  wrapper.classList.add('modal-overlay');
+  basketView.classList.add('basket-is-visible');
+}
+
+function closeBasket() {
+  if(isVisible) {
+    hideBasket();
   }
 
   isVisible = !isVisible;
 }
 
+function toggleBasket(e) {
+  if (isVisible) {
+    hideBasket();
+  } else {
+    showBasket();
+  }
+
+  e.stopPropagation();
+  isVisible = !isVisible;
+}
+
 document.getElementById('basket-logo').addEventListener('click', toggleBasket);
+document.body.addEventListener('click', closeBasket);
